bc08940cdd6f23bc51f41ed1f89a5a85b2241531
Assaduzzaman Assad <assaduzzaman.assad@yrbrands.com>
2017-12-18 15:46:18 +0600

    added thread

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+	 public void foo(String s) {
+	 System.out.println("String");
+	 }
+
+	 public void foo(StringBuffer sb){
+	 System.out.println("StringBuffer");
+	 }
+
+	 public static void main(String[] args) {
+		new Test().foo(null);
+	}
+}
