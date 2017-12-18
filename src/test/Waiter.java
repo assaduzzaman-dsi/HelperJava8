cb3970a16d0e7d7742a1f4f62a32346a7b45f1ed
Assaduzzaman Assad <assaduzzaman.assad@yrbrands.com>
2017-12-18 15:48:30 +0600

    added

--- src/test/Waiter.java
+++ src/test/Waiter.java
@@ -1,9 +1,27 @@
 package test;
 
-public class Waiter {
+public class Waiter implements Runnable{
+    
+    private Message msg;
+    
+    public Waiter(Message m){
+        this.msg=m;
+    }
 
-	public Waiter(Message msg) {
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        synchronized (msg) {
+            try{
+                System.out.println(name+" waiting to get notified at time:"+System.currentTimeMillis());
+                msg.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(name+" waiter thread got notified at time:"+System.currentTimeMillis());
+            //process the message now
+            System.out.println(name+" processed: "+msg.getMsg());
+        }
+    }
 
 }
